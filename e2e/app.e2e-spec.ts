e739d76ff6bfabe71d77a177c0d4bfcc4fa2842b
Angular CLI <angular-cli@angular.io>
2017-08-05 10:58:07 +0300

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { TodolistElectronPage } from './app.po';
+
+describe('todolist-electron App', () => {
+  let page: TodolistElectronPage;
+
+  beforeEach(() => {
+    page = new TodolistElectronPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
