8a5f2c628351651e463925423b95afde2893ea83
reuvec <chen.reuven@dell.com>
2018-04-08 22:10:41 +0300

    test to check prettier on html one file

--- src/app/app-todolist/todlist-footer/todlist-footer.component.ts
+++ src/app/app-todolist/todlist-footer/todlist-footer.component.ts
@@ -2,14 +2,17 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-todlist-footer',
-  templateUrl: './todlist-footer.component.html',
+  template: `
+    <footer class="footer">
+            <span class="todo-count"><strong>0</strong> items left</span>
+      <button class="clear-completed">Clear completed
+      </button>
+    </footer>
+  `,
   styleUrls: ['./todlist-footer.component.css']
 })
 export class TodlistFooterComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
