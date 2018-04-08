b5fe42edc352551a36a0e7fa4931026f06276897
reuvec <chen.reuven@dell.com>
2018-04-08 22:03:51 +0300

    test to check prettier on html one file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {AppTodolistModule} from './app-todolist/app-todolist.module';
+import { AppTodolistModule } from './app-todolist/app-todolist.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppTodolistModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppTodolistModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
