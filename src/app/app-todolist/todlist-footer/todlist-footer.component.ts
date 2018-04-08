4061a615d7d989a9a8ea6fd1b0b0a70b17738826
reuvec <chen.reuven@dell.com>
2018-04-08 22:11:50 +0300

    test to check prettier on html one file

--- src/app/app-todolist/todlist-footer/todlist-footer.component.ts
+++ src/app/app-todolist/todlist-footer/todlist-footer.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-todlist-footer',
   template: `
     <footer class="footer">
-            <span class="todo-count"><strong>0</strong> items left</span>
+      <span class="todo-count"><strong>0</strong> items left</span>
       <button class="clear-completed">Clear completed
       </button>
     </footer>
