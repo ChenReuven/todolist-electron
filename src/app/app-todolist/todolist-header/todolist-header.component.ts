8b962e7b4230ceb3efdf9e77a85950b621a434c4
reuvec <chen.reuven@dell.com>
2018-04-08 23:07:02 +0300

    test stylelint

--- src/app/app-todolist/todolist-header/todolist-header.component.ts
+++ src/app/app-todolist/todolist-header/todolist-header.component.ts
@@ -8,7 +8,9 @@ import { Component, Input, OnInit } from '@angular/core';
 export class TodolistHeaderComponent implements OnInit {
   @Input() title;
 
-  constructor() {}
+  constructor() {
+    var x = "s";
+  }
 
   ngOnInit() {}
 }
