2d5cbf2e887ca79c21c1bb4e270f0b56cb0476ed
reuvec <chen.reuven@dell.com>
2018-04-08 21:20:15 +0300

    insert husky and lint-stage

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('todolist-electron App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('todos');
   });
 });
